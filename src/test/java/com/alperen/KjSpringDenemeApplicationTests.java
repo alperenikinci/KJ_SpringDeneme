28f5ffc9edc9b489c74fb4a2133a430254f18094
alperenikinci <alperen.ikinci@hotmail.com>
2024-05-02 12:30:58 +0300

    Initial commit

---
+++ src/test/java/com/alperen/KjSpringDenemeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alperen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KjSpringDenemeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
